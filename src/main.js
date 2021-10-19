994b11e70272531c1ed8e0e618bf86eb10ba25b5
pqbao <baob1906431@student.ctu.edu.vn>
2021-10-19 15:45:12 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import router from "./router";
+import { handle } from "./common/promise";
 
-createApp(App).mount('#app')
+const app=createApp(App);
+app.use(router);
+app.mixin({methods: {handle}});
+app.mount("#app");
