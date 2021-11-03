ab2b1ccfd6389d17bfbe31f5fb13eed76ef51866
pqbao <baob1906431@student.ctu.edu.vn>
2021-11-03 23:55:16 +0700

    Tao store vuex luu tru du lieu

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from "./store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import router from "./router";
 import { handle } from "./common/promise";
 
 const app=createApp(App);
+app.use(store);
 app.use(router);
 app.mixin({methods: {handle}});
 app.mount("#app");
